5c7a28f2bd60ac1f30fa01157f5737b19825e6d9
Sergeysv1101 <146134888+Sergeysv1101@users.noreply.github.com>
2023-11-13 11:06:17 +0100

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'serhii';                 // Наш логин
-$mail->Password = 'pucpuwqmvnmtvhgy';                    // Наш пароль от ящика
+$mail->Username = 'sergeysv1101@gmail.com';                 // Наш логин
+$mail->Password = 'Home4110';                    // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('serhii', 'Pulse');   // От кого письмо 
+$mail->setFrom('sergeysv1101@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('ssurovec303@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
